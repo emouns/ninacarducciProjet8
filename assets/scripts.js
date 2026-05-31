429c690023079869323ca49bfa5e1bdea6577049
moun <mounirfendou@gmail.com>
2026-05-31 11:41:10 +0200

    modif   html /js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
+$(document).ready(function() {  //document attend que tout le html se charge  avant d'executer le code//
+    $('.gallery').mauGallery({  //cela appelle plugin sur le div gallery du fichier html //
+        columns: {              //va definir combien de collonne afficher selan la taille d'ecran//
             xs: 1,
             sm: 2,
             md: 3,
             lg: 3,
             xl: 3
         },
-        lightBox: true,
+        lightBox: true,         //agrandissement au clic//
         lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+        showTags: true,        // active les boutons filtres (mariage,concert.....)//
+        tagsPosition: 'top'    // place les filtres audessus de la galerie//
     });
 });
